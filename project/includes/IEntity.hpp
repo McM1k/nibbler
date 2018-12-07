2e448509c21c0937ccc23f3f423e9bb9b8b943ef
McM1k <gboudrie@student.42.fr>
2018-12-07 15:13:47 +0100

    removing most pure methods so all the logic will be in the main program

--- project/includes/IEntity.hpp
+++ project/includes/IEntity.hpp
@@ -6,10 +6,8 @@
 
 class IEntity {
 public:
-    virtual void display_board() = 0;
-    virtual void display_score(int score) = 0;
-    virtual void display_snake(std::list<Bloc> snake) = 0;
-    virtual void display_text(std::string str) = 0;
+    virtual void display_map(Map &map) const = 0;
+    virtual void display_text(std::string &str) const = 0;
 };
 
 
