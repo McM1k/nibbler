f238c0e13abbeb7803ccedebe59695fe93f3f3e2
McM1k <gallien.boudrie@gmail.com>
2018-12-09 12:50:16 +0100

    adding display and inputs

--- ncurses_library/includes/Display.hpp
+++ ncurses_library/includes/Display.hpp
@@ -1,21 +1,19 @@
 
 #ifndef DISPLAY_HPP
 # define DISPLAY_HPP
-
+# include "../../project/includes/IEntity.hpp"
+# include "../../project/includes/Map.hpp"
+# include "../../project/includes/UI.hpp"
 # include <iostream>
 
-class Display {
+class Display : public IEntity{
 public:
     Display(void) = default;
-
     Display(Display const &src) = default;
-
     virtual ~Display(void) = default;
-
     Display &operator=(Display const &rhs) = default;
 
-    std::ostream &operator<<(std::ostream &o);
-
+    void display(Map const &map, UI const &gameInfo) const;
 private:
 
 };
