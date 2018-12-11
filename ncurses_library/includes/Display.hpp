e47c3742d57de8cd5614b9a64c8181cad4f3e6e7
McM1k <gboudrie@student.42.fr>
2018-12-11 19:53:19 +0100

    added dew methods in Display, define for game objects

--- ncurses_library/includes/Display.hpp
+++ ncurses_library/includes/Display.hpp
@@ -2,22 +2,59 @@
 #ifndef DISPLAY_HPP
 # define DISPLAY_HPP
 
+# define GENERIC_BORDER '#'
+# define HORIZONTAL_BORDER
+# define VERTICAL_BORDER
+# define NW_CORNER_BORDER
+# define NE_CORNER_BORDER
+# define SE_CORNER_BORDER
+# define SW_CORNER_BORDER
+# define GRASS ' '
+# define FRUIT '\''
+# define OBSTACLE '@'
+# define GENERIC_SNAKE_PART '*'
+# define HORIZONTAL_SNAKE_PART
+# define VERTICAL_SNAKE_PART
+# define NW_SNAKE_PART
+# define NE_SNAKE_PART
+# define SE_SNAKE_PART
+# define SW_SNAKE_PART
+# define N_SNAKE_HEAD
+# define E_SNAKE_HEAD
+# define S_SNAKE_HEAD
+# define W_SNAKE_HEAD
+
 # include "../../project/includes/IEntity.hpp"
 # include "../../project/includes/Map.hpp"
 # include "../../project/includes/UI.hpp"
 # include <iostream>
+# include <ncurses.h>
 
 class Display : public IEntity{
 public:
     Display(void) = default;
     Display(Display const &src) = default;
-    virtual ~Display(void) = default;
+    virtual ~Display(void);
     Display &operator=(Display const &rhs) = default;
 
-    void display(Map const &map, UI const &gameInfo) const;
+    Display(int x, int y);
+
+    void display(Map const &map, UI const &gameInfo);
+
+    class UnableToDeleteWindowException : public std::exception {
+    public:
+        virtual const char *what() const throw();
+    };
 private:
+    void printSnake(const std::list<Bloc *> snake);
+    void printFruit(const Bloc *fruit);
+    void printObstacles(const std::list<Bloc *> &obstacles);
+
+    WINDOW      *window;
+    int         red_colour;
+    int         green_colour;
+    int         yellow_colour;
 
 };
-
 
 #endif
--- ncurses_library/sources/Display.cpp
+++ ncurses_library/sources/Display.cpp
@@ -4,7 +4,17 @@
 /* ******************************* */
 /*    Constructors & destructor    */
 /* ******************************* */
+Display::Display(int x, int y) {
+    this->window = newwin(x + 2, y + 7, 0, 0);
+    this->red_colour = (COLOR_RED, COLOR_BLACK);
+    this->yellow_colour = (COLOR_YELLOW, COLOR_BLACK);
+    this->green_colour = (COLOR_GREEN, COLOR_BLACK);
+}
 
+Display::~Display() {
+    if (!delwin(this->window))
+        throw UnableToDeleteWindowException();
+}
 /* ******************************* */
 /*            Accessors            */
 /* ******************************* */
@@ -16,10 +26,37 @@
 /* ******************************* */
 /*            Functions            */
 /* ******************************* */
-void Display::display(const Map &, const UI &) const {
-
+void Display::printSnake(const std::list<Bloc *> snake) {
+    for (Bloc *snake_part : snake){
+        mvaddch(snake_part->getY() + 1, snake_part->getX() + 1, GENERIC_SNAKE_PART);
+    }
 }
+
+void Display::printFruit(const Bloc *fruit) {
+    if (fruit->getX() >= 0 && fruit->getY() >= 0) {
+        mvaddch(fruit->getY() + 1, fruit->getX() + 1, FRUIT);
+    }
+}
+
+void Display::printObstacles(const std::list<Bloc *> &obstacles) {
+    for (Bloc *obstacle_part : obstacles){
+        mvaddch(obstacle_part->getY() + 1, obstacle_part->getX() + 1, OBSTACLE);
+    }
+}
+
+void Display::display(const Map &map, const UI &gameInfo) {
+    wclear(this->window);
+    wborder(this->window, GENERIC_BORDER, GENERIC_BORDER, GENERIC_BORDER, GENERIC_BORDER, GENERIC_BORDER, GENERIC_BORDER, GENERIC_BORDER, GENERIC_BORDER);
+    //wborder(this->window, HORIZONTAL_BORDER, HORIZONTAL_BORDER, VERTICAL_BORDER, VERTICAL_BORDER,
+    //        NW_CORNER_BORDER, NE_CORNER_BORDER, SW_CORNER_BORDER, SE_CORNER_BORDER);
+    printSnake(map.getSnake());
+    printObstacles(map.getObstacles());
+    printFruit(map.getFruit());
+}
+
 /* ******************************* */
 /*            Exceptions           */
 /* ******************************* */
-
+const char* Display::UnableToDeleteWindowException::what() const throw(){
+    return "Unable to delete nCurses window";
+}
